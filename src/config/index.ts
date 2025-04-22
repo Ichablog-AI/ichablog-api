212489b55e0074c988440c26e79236077da53b96
Angel S. Moreno <angelxmoreno@gmail.com>
2025-04-22 01:49:27 -0400

    feat: installed minio

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,4 @@
+import type { ClientOptions as MinioClientOptions } from 'minio';
 const appConfig = {
   nodeEnv: Bun.env.NODE_ENV ?? 'development',
   port: Number(Bun.env.PORT) || 3000,
@@ -5,13 +6,6 @@ const appConfig = {
   databaseUrl: Bun.env.DATABASE_URL ?? '',
 
   jwtSecret: Bun.env.JWT_SECRET ?? '',
-
-  minio: {
-    endpoint: Bun.env.MINIO_ENDPOINT ?? '127.0.0.1',
-    port: Number(Bun.env.MINIO_PORT) || 9000,
-    accessKey: Bun.env.MINIO_ACCESS_KEY ?? '',
-    secretKey: Bun.env.MINIO_SECRET_KEY ?? '',
-  },
 
   meilisearch: {
     host: Bun.env.MEILISEARCH_HOST ?? 'http://127.0.0.1:7700',
@@ -21,6 +15,17 @@ const appConfig = {
   redisUrl: Bun.env.REDIS_URL ?? 'redis://127.0.0.1:6379',
   logger: {
     level: String(Bun.env.LOGGER_LEVEL ?? 'info'),
+  },
+  minio: {
+    clientOptions: {
+      useSSL:
+        String(Bun.env.MINIO_USE_SSL) === 'true' ||
+        Number(Bun.env.MINIO_USE_SSL) === 1,
+      endPoint: String(Bun.env.MINIO_ENDPOINT ?? '127.0.0.1'),
+      port: Number(Bun.env.MINIO_PORT || 9000),
+      accessKey: String(Bun.env.MINIO_ACCESS_KEY ?? 'ichablog'),
+      secretKey: String(Bun.env.MINIO_SECRET_KEY ?? 'ichablog'),
+    } as MinioClientOptions,
   },
 };
 
