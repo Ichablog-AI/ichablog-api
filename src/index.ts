a62e4c4f0c9ba51e1a0b01fa993569a591a4c842
Angel S. Moreno <angelxmoreno@gmail.com>
2025-04-25 03:00:53 -0400

    feat(http-server): tied http-server to Bun.serve with props

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log('Hello via Bun!');
+import 'reflect-metadata';
+import appConfig from '@be/config';
+import { appContainer } from '@be/config/container';
+import { AppDataSource } from '@be/database/data-source';
+import { createApp } from '@be/server';
+import { LoggerRegistry } from '@be/services/LoggerRegistry';
+
+const logger = appContainer.resolve(LoggerRegistry).getLogger('AppBootstrap');
+
+(async () => {
+    try {
+        logger.info('Initializing database...');
+        await AppDataSource.initialize();
+        logger.info('Database initialized.');
+
+        logger.info('Initializing application...');
+        const app = createApp();
+        Bun.serve({
+            hostname: appConfig.httpServer.host,
+            port: appConfig.httpServer.port,
+            fetch: app.fetch,
+        });
+        logger.info('Application initialized.');
+
+        logger.info(
+            `🟢 Listening on ${appConfig.httpServer.protocol}://${appConfig.httpServer.host}:${appConfig.httpServer.port}`
+        );
+    } catch (error) {
+        logger.error(error);
+    }
+})();
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,7 @@
-import appConfig from '@be/config';
 import { Hono } from 'hono';
 
-const app = new Hono();
-
-app.get('/', (c) => c.text('Hello Bun!'));
-
-const serverConfig = {
-    port: appConfig.port,
-    fetch: app.fetch,
-    request: app.request,
-};
-
-if (import.meta.main) {
-    // This tells Bun to start the HTTP server when you run this file directly
-    Bun.serve(serverConfig);
-    console.log(`🟢 Listening on http://localhost:${appConfig.port}`);
+export function createApp() {
+    const app = new Hono();
+    app.get('/', (c) => c.text('Hello Bun!'));
+    return app;
 }
-
-export default serverConfig;
